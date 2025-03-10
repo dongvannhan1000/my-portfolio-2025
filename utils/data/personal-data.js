5bd55f5a2215260d4545b5c38731e1b2b17cc446
Nhan Dong <nhandong0205@gmail.com>
2025-03-10 22:53:13 +0700

    adjust personal data

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "NHAN DONG",
   profile: '/profile.jpg',
   designation: "Web Developer",
-  description: "My name is NHAN DONG. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "Hello, I'm NHAN DONG – a dedicated web developer with a passion for problem-solving and continuous learning. With a strong foundation in JavaScript and full-stack development, I enjoy exploring new technologies to build modern, scalable, and user-friendly web applications. My self-driven approach and quick learning ability enable me to adapt and innovate, making the web a more open and dynamic space. I’m excited to connect on opportunities that align with my skills and ambition.",
   email: 'nhandong0205@gmail.com',
   phone: '0971268043',
   address: 'District Tan Binh, Ho Chi Minh City, Vietnam ',
@@ -13,5 +13,5 @@ export const personalData = {
   // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
   // leetcode: "https://leetcode.com/said3812/",
   // devUsername: "said7388",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1D8ocDjterpce-xsYHEmpux-S78gxuYT7/view?usp=sharing"
 }
