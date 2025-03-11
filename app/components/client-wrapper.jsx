6ac2f118178dbabc49b29cb15de5868fe5127ad6
Nhan Dong <nhandong0205@gmail.com>
2025-03-11 22:27:50 +0700

    try client wrapper with ssr true

--- app/components/client-wrapper.jsx
+++ app/components/client-wrapper.jsx
@@ -3,11 +3,15 @@
 import dynamic from 'next/dynamic';
 
 // Dynamically import all components with SSR: false
-export const HeroSection = dynamic(() => import("./homepage/hero-section"), { ssr: false });
-export const AboutSection = dynamic(() => import("./homepage/about"), { ssr: false });
-export const Experience = dynamic(() => import("./homepage/experience"), { ssr: false });
-export const Skills = dynamic(() => import("./homepage/skills"), { ssr: false });
-export const Projects = dynamic(() => import("./homepage/projects"), { ssr: false });
+
+export const AboutSection = dynamic(() => import("./homepage/about"), { ssr: true });
+export const Blog = dynamic(() => import("./homepage/blog"), { ssr: true });
+export const ContactSection = dynamic(() => import("./homepage/contact"), { ssr: true });
 export const Education = dynamic(() => import("./homepage/education"), { ssr: false });
-export const ContactSection = dynamic(() => import("./homepage/contact"), { ssr: false });
-export const Blog = dynamic(() => import("./homepage/blog"), { ssr: false });
+export const Experience = dynamic(() => import("./homepage/experience"), { ssr: false });
+export const HeroSection = dynamic(() => import("./homepage/hero-section"), { ssr: true });
+export const Projects = dynamic(() => import("./homepage/projects"), { ssr: false });
+export const Skills = dynamic(() => import("./homepage/skills"), { ssr: false });
+
+
+
